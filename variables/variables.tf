c6f7e7fc02ac93cfe1688ffd0dcc883c6656e195
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-20 06:10:28 +0000

    adding modified file

--- variables/variables.tf
+++ variables/variables.tf
@@ -2,10 +2,22 @@ variable sample {
   default = "Hello"
 }
 
+variable sample3 {
+  default = 123
+}
+
+variable sample4 {
+  A = true
+}
+
 output sample1 {
-  value = var.sample
+  value = var.sample3
 }
 
 output sample2 {
   value = "${var.sample} everyone, welcome to the session"
+}
+
+output sample1 {
+  value = var.sample4
 }
