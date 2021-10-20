3f4c403ab744dafb300b4d7ba98efd00d9cd3410
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-20 06:42:14 +0000

    adding output file

--- variables/variables.tf
+++ variables/variables.tf
@@ -19,11 +19,11 @@ variable A {
 }
 
 output sample1 {
-  value = var.sample
+  value = var.sample[*]
 }
 
 output sample2 {
-  value = "${var.sample} everyone, welcome to the session"
+  value = "${var.sample[*]} everyone, welcome to the session"
 }
 
 output sample5 {
