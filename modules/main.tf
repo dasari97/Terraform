c9d5d3d3856e916470dc5fce293fb8bdd304048f
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-21 09:39:54 +0000

    adding new file

---
+++ modules/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "ec2" {
+    source = "./module"
+}
+
+module "sg" {
+    source = "./sg"
+}
---
+++ modules/sg/sg.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "instance1" {
+  ami                    = "ami-0e4e4b2f188e91845"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.A.id]
+  tags                   = {
+    Name                 = "instance1"
+  }
+}
+
+resource "aws_security_group" "A" {
+  name        = "terraform"
+  description = "Allow TLS inbound traffic"
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
+    }
+  ]
+
+  egress = [
+    {
+      description      = "ALL"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
+    }
+  ]
+
+  tags = {
+    Name = "new"
+  }
+}
