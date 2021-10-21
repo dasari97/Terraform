b84252bad5ad127cfc5b9d60bb0f593c38cba7c0
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-21 09:10:22 +0000

    adding new file

--- ec2_with_sg/ec2.tf
+++ ec2_with_sg/ec2.tf
@@ -2,6 +2,14 @@ provider "aws" {
     region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "dasari1998"
+    key    = "Terraform/terraform_statefile"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_instance" "instance1" {
   ami           = "ami-0e4e4b2f188e91845"
   instance_type = "t2.micro"
