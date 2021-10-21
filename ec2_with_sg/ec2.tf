94c4090e30ab9e921ae21d193a4c5a578a0200ab
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-21 08:46:42 +0000

    adding new file

--- ec2_with_sg/ec2.tf
+++ ec2_with_sg/ec2.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "A" {
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
+      ipv6_cidr_blocks = []
       prefix_list_ids  = []
       self             = false
       security_groups  = []
@@ -33,7 +33,7 @@ resource "aws_security_group" "A" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
+      ipv6_cidr_blocks = []
       prefix_list_ids  = []
       self             = false
       security_groups  = []
