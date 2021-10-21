5629ef682c6dbd3faadefa82bb010382b5de192b
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-21 06:01:35 +0000

    adding new file

---
+++ ec2 with sg/ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "instance1" {
+  ami           = ami-0e4e4b2f188e91845
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "instance1"
+  }
+}"
