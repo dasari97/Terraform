f54844a02fe1597d990d95d3787744199ee7a3e5
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-21 09:43:51 +0000

    adding new file

--- modules/sg/sg.tf
+++ modules/sg/sg.tf
@@ -1,11 +1,3 @@
-resource "aws_instance" "instance1" {
-  ami                    = "ami-0e4e4b2f188e91845"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.A.id]
-  tags                   = {
-    Name                 = "instance1"
-  }
-}
 
 resource "aws_security_group" "A" {
   name        = "terraform"
