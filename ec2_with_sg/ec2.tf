73119a4dcf7fffe3479ec13311d39494a859740b
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-21 06:59:34 +0000

    adding new file

--- ec2_with_sg/ec2.tf
+++ ec2_with_sg/ec2.tf
@@ -5,4 +5,42 @@ provider "aws" {
 resource "aws_instance" "instance1" {
   ami           = "ami-0e4e4b2f188e91845"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.new.id]
+}
+
+resource "aws_security_group" "new" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      self             = false
+      security_groups  = []
+    }
+  ]
+
+  tags = {
+    Name = "new"
+  }
 }
