aef9362b15bb21eac1c58a0637d79ece23641cce
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-21 07:40:18 +0000

    adding new file

--- ec2_with_sg/ec2.tf
+++ ec2_with_sg/ec2.tf
@@ -5,10 +5,10 @@ provider "aws" {
 resource "aws_instance" "instance1" {
   ami           = "ami-0e4e4b2f188e91845"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.1.id]
+  vpc_security_group_ids = [aws_security_group.A.id]
 }
 
-resource "aws_security_group" "1" {
+resource "aws_security_group" "A" {
   name        = "terraform"
   description = "Allow TLS inbound traffic"
 
